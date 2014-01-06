d6ae04bba95684920d7794b7e3f9143f4c48c127
Sean Mongey <sm@smongey.com>
2014-01-06 20:30:15 +0000

    news page and various other tweaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -67,11 +67,35 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 
 			});
 
+
+		// SINGLE PROJECT
+		} else if (location.pathname.indexOf('project') > -1) {
+
+			$(window).scrollTo('#page', 0, function(){
+
+				$('#nav').fadeOut(0, function(){
+					$(this).removeClass().fadeIn(0);
+					$('a.introToggle').removeClass('locked');
+				});
+				
+				$('#intro').animate({ 'margin-top': 0 }, 0);
+				$('#page').animate({ 'margin-top': $(window).height() }, 0);
+				$('#second').fadeOut(0);
+				$('div.bg').css({'background-position-y' : '0' });
+				$('#first li:not(:last-child) a').removeClass('active');
+				
+				$('body').removeClass('hidden');
+				$('.project .text').fadeIn(0);
+				$('#first li:first-child a').addClass('active');
+
+			});
+			textToggle();
+
 		// PRACTICE
 		} else if (location.pathname.indexOf('practice') > -1) {
 
 			$(window).scrollTo('#page', 0, function(){
-
+				
 				$('#nav').fadeOut(0, function(){
 					$(this).removeClass().fadeIn(0);
 					$('a.introToggle').removeClass('locked');
@@ -84,14 +108,33 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 				$('body').removeClass('hidden');
 				$('.practice ul.menu').fadeIn(0);
 				$('#first li:nth-child(2) a').addClass('active');
-				practiceMenu();
-
+				
 			});
+
+			practiceMenu();
 
 		// NEWS
 		} else if (location.pathname.indexOf('news') > -1) {
 
-			console.log('news');
+
+			$(window).scrollTo('#page', 0, function(){
+				
+				$('#nav').fadeOut(0, function(){
+					$(this).removeClass().fadeIn(0);
+					$('a.introToggle').removeClass('locked');
+				});
+				
+				$('#intro').animate({ 'margin-top': 0 }, 0);
+				$('#page').animate({ 'margin-top': $(window).height() }, 0);
+				$('div.bg').css({'background-position-y' : '0' });
+				$('#first li:not(:last-child) a').removeClass('active');
+				$('body').removeClass('hidden');
+				$('#first li:nth-child(3) a').addClass('active');
+
+				
+			});
+			newsTitle();
+
 
 		}
 
@@ -103,7 +146,7 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 
 
 
-	$('a.introToggle').click(function(e){
+	$('a.introToggle').on('click', function(e){
 		e.preventDefault();
 
 
@@ -124,6 +167,7 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 					$('#page').animate({ 'margin-top': $(window).height() }, 400);
 					$('#second').fadeOut(100);
 					$('.practice ul.menu').fadeOut(100);
+					$('.project .text').fadeOut(100);
 					$('div.bg').css({'background-position-y' : '0' });
 					
 				});
@@ -135,6 +179,7 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 				$('#page').animate({ 'margin-top': $(window).height() }, 400);
 				$('#second').fadeOut(100);
 				$('.practice ul.menu').fadeOut(100);
+				$('.project .text').fadeOut(100);
 				$('div.bg').css({'background-position-y' : '0' });
 				
 				$('#nav').fadeOut(300, function(){
@@ -143,7 +188,7 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 				});
 
 				scrollIntro();
-				slideTitlePosition();
+				slideTitlePosition('#slider', '.bg h2');
 
 			}
 
@@ -161,33 +206,21 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 
 					$('#second').removeClass('home').fadeIn(300);
 
+				} else if (location.pathname.indexOf('project') > -1) {
+
+					$('.project .text').fadeIn(300);
+				
 				} else if (location.pathname.indexOf('practice') > -1) {
 
 					$('.practice ul.menu').fadeIn(300);
 
 				}
-
-
 			});
-
 		}
-
 	});
 
-
-	$('#first li:not(:last-child) a, a.logo').click(function(e){
-		e.preventDefault();
-
-		var address = $(this).attr('href');
-		history.pushState(address, '', address);
-
-		$('#first li:not(:last-child) a').removeClass('active');
-		$(this).addClass('active');
-
-		ajaxCall(address);
-
-	});
-
+	ajaxLink('#first li:not(:last-child) a, a.logo');
+	ajaxLink('a.single, a.back');
 
 	$(window).on('resize', function() {
 		//slideTitlePosition();
@@ -199,27 +232,11 @@ require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'isotope', 'wa
 
 }); // end require
 
-// Load isotope
-var isotopeLoad = function(item, gutter, column){
 
-	console.log('isotope loaded');
-
-	//isotope
-	$('#container').isotope({
-		itemSelector: item,
-		masonry: {
-			gutterWidth: gutter,
-			columnWidth: column
-			//cornerStampSelector: '.stamp'
-		}
-	});
-
-	centreThumbs(item);};
-
-// Ajax Call
+// Ajax call
 var ajaxCall = function(address){
 
-	if (address.indexOf('/') > -1) {
+	if (address.indexOf('/') > -1 || window.location.href === 'http://127.0.0.1:9000/' || window.location.href === 'http://smongey.github.io/landl/') {
 		
 		$(window).scrollTo(0, 300, function(){
 			$('#page > *').fadeOut(500, function(){
@@ -227,8 +244,9 @@ var ajaxCall = function(address){
 					
 					$(this).fadeIn('slow');
 					isotopeLoad('.item', 60, 100);
-
 					$('#second').fadeOut(300);
+					
+					console.log('home ajax load');
 					
 				});
 			});
@@ -240,24 +258,69 @@ var ajaxCall = function(address){
 		$(window).scrollTo('#page', 300, function(){
 			$('#page > *').fadeOut(500, function(){
 				$('#page').hide().load(address + ' #page > *', function(){
-					$(this).fadeIn('slow');
+					$(this).fadeIn(300);
+
 					if (address.indexOf('projects') > -1) {
+					
 						$('#second').hide().removeClass('home').fadeIn(300);
 						$('.practice ul.menu').fadeOut(300);
 						isotopeLoad('.item', 60, 100);
+						ajaxLink('a.single');
+						console.log('projects ajax load');
+					
+					} else if (address.indexOf('project') > -1) {
+					
+						$('#second').fadeOut(300);
+						$('.practice ul.menu').fadeOut(300);
+						ajaxLink('a.back');
+						textToggle();
+						console.log('project ajax load');
+
 					} else if (address.indexOf('practice') > -1) {
+						
 						$('#second').fadeOut(300);
 						$('.practice ul.menu').fadeIn(300);
 						practiceMenu();
-					} else {
+						console.log('practice ajax load');
+							
+						
+					} else if (address.indexOf('news') > -1) {
+						
 						$('#second').fadeOut(300);
+						newsTitle();
+						console.log('news ajax load');
+
+					} else {
+					
+						$('#second').fadeOut(300);
+						console.log('default ajax load');
+					
 					}
+
 				});
 			});
 		});
-	}};
+	}
+};
 
-// Centre thumbnail titles and images
+// Load isotope
+var isotopeLoad = function(item, gutter, column){
+
+	$('#container').imagesLoaded( function(){
+		$('#container').fadeIn(1000).isotope({
+			itemSelector: item,
+			masonry: {
+				gutterWidth: gutter,
+				columnWidth: column
+				//cornerStampSelector: '.stamp'
+			}
+		});
+		centreThumbs(item);
+	});
+	console.log('isotope loaded');
+};
+
+// Centre thumbnail titles and images on projects page
 var centreThumbs = function(item) {
 	
 	$(item).each(function() {
@@ -284,30 +347,35 @@ var centreThumbs = function(item) {
 			});
 		}
 
-	});};
+	});
+	console.log('centred thumbs');
+};
 
-// Load slider
+// Centre intro slider titles
+var slideTitlePosition = function(elem, title) {
+	var halfHeight = $(elem).height() / 2 - 30;
+	$(title).css({
+		'margin-top': halfHeight
+	});
+};
+
+// Intro slider init
 var sliderLoad = function(id, speed){
 	window.mySwipe = new Swipe(document.getElementById(id), {
 		startSlide: 0,
 		speed: speed,
-		continuous: true,
+		continuous: false,
 		disableScroll: false,
 		stopPropagation: false,
 		callback: function(index, elem) {},
 		transitionEnd: function(index, elem) {}
 	});
 
-	slideTitlePosition();};
+	slideTitlePosition('#slider', '.bg h2');
+};
 
-// Centre slider titles
-var slideTitlePosition = function() {
-	var halfHeight = $('#slider').height() / 2 - 30;
-	$('.bg h2').css({
-		'margin-top': halfHeight
-	});};
 
-// on scroll intro action
+// Scroll watching intro toggler
 var scrollIntro = function(){
 
 	$(window).on('scroll', function() {
@@ -316,7 +384,7 @@ var scrollIntro = function(){
 		var slideTitlePos = $('#slider').height() / 2 - 30;
 
 		$('.bg').css({'background-position-y' : -(pageOffset / 5) });	
-		slideTitlePosition(/*slideTitlePos + (pageOffset / 10)*/);
+		slideTitlePosition('#slider', '.bg h2'/*slideTitlePos + (pageOffset / 10)*/);
 
 		
 
@@ -347,16 +415,73 @@ var scrollIntro = function(){
 			$('#nav').removeClass('on');
 		}
 
-	});};
+	});
+};
 
-// scrolling on practice page
+// localscroll and practice nav highlighting
 var practiceMenu = function(){
+	$('ul.menu').localScroll({ 
+		duration: 600, 
+		offset: -80, 
+		hash: false, 
+		lazy: true 
+	});
 
-	$('ul.menu').localScroll({ duration: 600, offset: -80 });
 	$('ul.menu li a').each(function() {
 		$(this).click(function(){
 			$('ul.menu li a').removeClass('active');
 			$(this).addClass('active');
 		});
 	});
-	$('ul.menu li:first-child a').addClass('active');};
+
+	$('ul.menu li:first-child a').addClass('active');
+	
+	console.log('practiceMenu init');
+	
+};
+
+// ajax for the link
+var ajaxLink = function(elem){
+	console.log('ajaxLink init');
+	$(elem).on('click', function(e){
+		e.preventDefault();
+		var address = $(this).attr('href');
+		history.pushState(address, '', address);
+		$('#first li:not(:last-child) a').removeClass('active');
+		if (elem == 'a.single') {
+			$('#first li:first-child a').addClass('active');
+		} else {
+			$(this).addClass('active');
+		}
+		
+		ajaxCall(address);
+
+	});
+};
+
+var textToggle = function(){
+
+	$('a.info').on('click', function(e){
+		e.preventDefault();
+		if (!$(this).hasClass('on')) {
+			$(this).addClass('on').siblings('div, a.back').addClass('hide');
+			$(this).empty().append('Show info');
+			$('.images').addClass('large');
+		} else {
+			$(this).removeClass('on').siblings('div, a.back').removeClass('hide');
+			$(this).empty().append('Hide info');
+			$('.images').removeClass('large');
+		}
+	});
+
+};
+
+var newsTitle = function(){
+	var imageWidth = $('.full div.image').width() / 2;
+	var titleWidth = $('.image h2').outerWidth() / 2;
+	$('.image h2').css({'margin-left': (imageWidth - titleWidth) });
+	slideTitlePosition('.full div.image', '.image h2');
+	console.log('newsTitle init');
+	$('.image h2').animate({'opacity': 1}, 300);
+};
+
