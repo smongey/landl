95925e4e6b33a97c938a2b1309c1e1efab1940da
Sean Mongey <sm@smongey.com>
2014-02-11 18:20:27 +0000

    -n attempted to fix news section

--- site/templates/news.php
+++ site/templates/news.php
@@ -7,62 +7,52 @@ include("./head.inc");
 			
 			<div id="container">
 
-				<div class="full">
-
-					<?php 
-					$news = $pages->get(1011); 
-					$first = $news->children->first();
-					$img = $first->news_image->first();
-					$name = $first->news_author->name;
-					$u = $users->get('$name'); 
-					//$photo = $u->person_photo->first()->size(100,100);
-					?>
-
-					<div class="image">
-						<h2><?php echo "{$first->title}"; ?></h2>
-						<img src="<?php echo "{$img->url}"; ?>" alt=''>
-					</div>
-					<div class="text">
-						<div class="profile">
-							<img src="<?php echo "{$u->title}" ?>" alt="">
-							<p>Published on <span><?php echo date("j M Y", $first->created); ?></span> by <span><?php echo "{$first->news_author->name}"; ?></span></p>
-						</div>
-						<div class="intro">
-							<p><?php echo "{$first->news_excerpt}"; ?></p>
-							<a href="<?php echo "{$first->url}"; ?>" class="more"></a>
-						</div>
-					</div>
-				</div>
-
-
 				<?php 
 				$num = 0;
-				$posts = $pages->find('template=entry, start=2'); 
+				$posts = $pages->find('template=entry'); 
 					
 
 					foreach($posts as $post) {
 						$name = $post->news_author->name;
-						$u = $users->get('$name');
-						//$profile = $u->person_photo->first()->size(50,50); 
-						
+						$u = $users->get($name);
+						$profile = $u->person_photo->size(50,50); 
+						$mainprofile = $u->person_photo->size(100,100); 
+						$img = $post->news_image->first()->size(540,400);
+						$main = $post->news_image->first()->size(1000,500);
+
 						$num = $num + 1;
 						
-						if($num == 1) {
-						
-							// dont show the first post as it's already above
+						if($num == 1) { ?>
+									
+							<div class="full">
+								<div class="image">
+									<h2><?php echo "{$post->title}"; ?></h2>
+									<img src="<?php echo "{$main->url}"; ?>" alt=''>
+								</div>
+								<div class="text">
+									<div class="profile">
+										<img src="<?php echo "{$mainprofile->url}" ?>" alt="">
+										<p>Published on <span><?php echo date("j M Y", $post->created); ?></span> by <span><?php echo "{$post->news_author->name}"; ?></span></p>
+									</div>
+									<div class="intro">
+										<p><?php echo "{$post->news_excerpt}"; ?></p>
+										<a href="<?php echo "{$post->url}"; ?>" class="more"></a>
+									</div>
+								</div>
+							</div>
 
-						} elseif ($num % 2 == 0) { ?>
+						<?php } elseif ($num % 2 == 0) { ?>
 
 							<div class="left">
 								<div class="text">
 									<span class="date"><?php echo date("j M Y", $post->created); ?></span>
-									<img src="<?php echo "{$u->name}" ?>" alt="">
-									<h3><?php echo $post->title ?></h3>
+									<img src="<?php echo "{$profile->url}" ?>" alt="">
+									<h3><?php echo $post->title; ?></h3>
 									<p><?php echo $post->news_excerpt ?></p>
-									<a href="<?php echo "{$post->url}" ?>" class="more"></a>
+									<a href="<?php echo $post->url ?>" class="more"></a>
 								</div>
 								<div class="image">
-									<img src="<?php echo $post->news_image->first()->size(540, 400)->url; ?>" alt="">
+									<img src="<?php echo "{$img->url}" ?>" alt="">
 								</div>
 							</div>
 							
@@ -70,18 +60,19 @@ include("./head.inc");
 
 							<div class="right">
 								<div class="image">
-									<img src="<?php echo $post->news_image->first()->size(540, 400)->url; ?>" alt="">
+									<img src="<?php echo "{$img->url}" ?>" alt="">
 								</div>
 								<div class="text">
 									<span class="date"><?php echo date("j M Y", $post->created); ?></span>
-									<img src="<?php echo "{$u->person_photo->url}" ?>" alt="">
-									<h3><?php echo $post->title ?></h3>
+									<img src="<?php echo "{$profile->url}" ?>" alt="">
+									<h3><?php echo $post->title; ?></h3>
 									<p><?php echo $post->news_excerpt ?></p>
-									<a href="<?php echo "{$post->url}" ?>" class="more"></a>
+									<a href="#" class="more"></a>
 								</div>
 							</div>
 
-						<?php } } ?>
+						<?php } 
+					} ?>
 
 			</div> 
 					
